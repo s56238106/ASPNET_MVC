ad62c68b7bb4a3a2b825aed50fb9f447cf59c54b
john <john@MSI>
2017-03-10 15:48:43 +0800

    ASPNET_MVC_0310_homework_2

---
+++ eSale/eSale/Models/OrderService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSale.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+        /// <summary>
+        /// 刪除訂單
+        /// </summary>
+        public void DeleteOrderById()
+        {
+
+        }
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(String id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚資訊";
+            return result;
+
+        }
+
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+
+
+
+    }
+}
