f926bf0309d65b17191da1ecd9fedbb5adab5bae
john <john@MSI>
2017-03-17 16:19:13 +0800

    0317_class

---
+++ eSale/eSale/Controllers/LayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSale.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: Layout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- eSale/eSale/Controllers/OrderController.cs
+++ eSale/eSale/Controllers/OrderController.cs
@@ -11,10 +11,13 @@ namespace eSale.Controllers
         // GET: Order
         public ActionResult Index()
         {
-            Models.OrderService orderService = new Models.OrderService();
-            var order = orderService.GetOrderById("111");
-            ViewBag.CustId = order.CustId;
-            ViewBag.CustName = order.CustName;
+            ///Models.OrderService orderService = new Models.OrderService();
+            ///var order = orderService.GetOrderById("111");
+            ///ViewBag.CustId = order.CustId;
+            ///ViewBag.CustName = order.CustName;
+
+            
+
             return View();
         }
 
@@ -37,10 +40,30 @@ namespace eSale.Controllers
         [HttpPost()]
         public ActionResult InsertOrder(Models.Order order)
         {
-            Models.OrderService orderService = new Models.OrderService();
-            orderService.InsertOrder(order);
-            return View("index");
+           /// Models.OrderService orderService = new Models.OrderService();
+           /// orderService.InsertOrder(order);
+
+            ViewBag.Desc1 = "我是ViewBag";
+            ViewData["Desc2"] = "我是ViewData";
+            TempData["Desc3"] = "我是TempData";
+            return RedirectToAction("index");
+
+           /// return View("index");
         }
+
+        [HttpGet]
+        public JsonResult TestJson()
+        {
+
+            ///var result = new Models.Order();
+            ///result.CustId = "GSS";
+            ///result.CustName = "叡揚資訊";
+
+            var result = new Models.Order(){CustId = "Gss",CustName = "叡揚資訊"};
+
+            return this.Json(result,JsonRequestBehavior.AllowGet);
+        }
+
 
 
     }
