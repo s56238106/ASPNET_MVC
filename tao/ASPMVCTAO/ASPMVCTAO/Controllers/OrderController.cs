169cf294eecea84f0384a9e398c9b58c9bb7911c
john <john@MSI>
2017-03-27 22:10:34 +0800

    3/27

---
+++ tao/ASPMVCTAO/ASPMVCTAO/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVCTAO.Controllers
+{
+    public class OrderController
+    {
+        //
+        //GET: /Order/
+
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+    }
+}
